b429f2004298dbf31a1bf51d875215015c3b73a9
Jeyson Correa <jgcorrea@contratista.icfes.gov.co>
2017-12-27 22:19:26 -0500

    Jeyson:COrreaccion de eliminar sessio

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   resources :libros
   devise_for :users
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
